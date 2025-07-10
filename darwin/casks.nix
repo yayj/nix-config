391d2c7541242f836d92cb3776d7ef4cb70c5717
Qiru Yang <yayjsir@gmail.com>
2025-07-10 14:15:27 -0400

    Add following applications for macos:
    - loop
    - tor-browser
    - Windows App
    - Xcode

--- darwin/casks.nix
+++ darwin/casks.nix
@@ -7,13 +7,17 @@ _: {
     "karabiner-elements"
     "keepingyouawake"
     "kitty"
+    "loop"
     "raycast"
     "snipaste"
     "squirrel"
+    "tor-browser"
   ];
 
   homebrew.masApps = {
     "Bitwarden" = 1352778147;
     "Hidden Bar" = 1452453066;
+    "Windows App" = 1295203466;
+    "Xcode" = 497799835;
   };
 }
