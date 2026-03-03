4abd7028a034b88ae44e303ae7ea045b1c2b68a9
Qiru Yang <yayjsir@gmail.com>
2026-03-03 09:47:11 -0500

    Upgrade NIX to 25.11

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Nix Configurations of Qiru Yang";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
 
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
