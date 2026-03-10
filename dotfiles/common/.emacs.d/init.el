a67b823d380e8c8baf022c377c8eae0718ab3516
Qiru Yang <yayjsir@gmail.com>
2026-03-10 10:38:24 -0400

    Replace Emacs package manager with straight.el

--- dotfiles/common/.emacs.d/init.el
+++ dotfiles/common/.emacs.d/init.el
@@ -43,21 +43,25 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
+;; Initialize straight.el as the package manager
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-;; Initialize package sources
-(require 'package)
-(setopt package-archives '(("melpa" . "https://melpa.org/packages/")
-                           ("elpa" . "https://elpa.gnu.org/packages/")))
-
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-(require 'use-package)
-(setopt use-package-always-ensure t)
+(straight-use-package 'use-package)
+(setopt straight-use-package-by-default t)
 
 ;; Packages
 
