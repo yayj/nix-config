c3bf3ef8422120e59b95579d3491b071771368e5
Qiru Yang <yayjsir@gmail.com>
2026-06-21 22:08:02 -0400

    Remove .gnupg package

--- dotfiles/darwin/.gnupg/pinentry.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-if (( $+commands[pinentry-mac] )); then
-  exec pinentry-mac "$@"
-elif (( $+commands[pinentry-tty] )); then
-  exec pinentry-tty "$@"
-else
-  exec pinentry "$@"
-fi
